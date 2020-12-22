e8e1b3d08666b25f8c420c0f99bb0baac4b1f3e7
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-12-22 14:22:13 -0700

    shapes for drawing

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-let size = 20;
+let size = 20
+let color = 'black'
+let x
+let y
 
 function drawCircle(x, y) {
   context.beginPath();
   context.arc(x, y, size, 0, Math.PI * 2, true)
+  context.fillStyle = color
 }
+
+function drawLine(x1, y1, x2, y2) {
+  context.beginPath()
+  context.moveTo(x1, y1)
+  context.lineTo(x2, y2)
+  context.strokeStyle = color
+  context.lineWidth = size
+  context.stroke()
+}
+

61d0fa1cdc70e1c49611ceb4dcff5dd6a6490d10
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-12-22 19:18:43 -0700

    back around again

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+});
+
+app.use(express.static(__dirname + '/'))
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000')
+})
